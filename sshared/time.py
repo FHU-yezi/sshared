1538b5d6687313e3b801eec96f841fba5b52e5de
yezi <yehaowei20060411@qq.com>
2024-05-04 09:06:15 +0800

    feat: time 模块新增 `get_now_without_microsecond` 函数

--- sshared/time.py
+++ sshared/time.py
@@ -3,3 +3,7 @@ from datetime import date, datetime
 
 def get_today_as_datetime() -> datetime:
     return datetime.fromisoformat(date.today().isoformat())
+
+
+def get_now_without_microsecond() -> datetime:
+    return datetime.now().replace(microsecond=0)
