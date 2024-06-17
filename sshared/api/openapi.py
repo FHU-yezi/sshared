3cb0431703f5c929dbfc331f283e2f55870ebee9
yezi <yehaowei20060411@qq.com>
2024-06-17 23:33:29 +0800

    fix: 通过覆盖 `openapi.json` 文件查找逻辑修复 API 文档无法展示的问题

--- sshared/api/openapi.py
+++ sshared/api/openapi.py
@@ -3,9 +3,19 @@ from typing import Any, Dict
 from litestar.openapi.plugins import ScalarRenderPlugin
 
 
+# 由于 Scalar Plugin 实现逻辑中硬编码了 openapi.json 文件的路径
+# 导致 API 通过反代暴露于 /api 路径下时 Scalar 无法正常找到 OpenAPI Schema
+# 因此造成了文档无法展示，此处通过覆盖 openapi.json 查找逻辑解决此问题
+class CustomScalarRenderPlugin(ScalarRenderPlugin):
+    @staticmethod
+    def get_openapi_json_route(request: Any) -> str:  # noqa: ANN401
+        del request
+        return "/api/docs/openapi.json"
+
+
 def generate_openapi_config_params() -> Dict[str, Any]:
     return {
         "path": "/docs",
-        "render_plugins": (ScalarRenderPlugin(path="/"),),
+        "render_plugins": (CustomScalarRenderPlugin(path="/"),),
         "use_handler_docstrings": True,
     }
