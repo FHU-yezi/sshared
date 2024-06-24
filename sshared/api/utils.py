9e365e857ef44b12c3526357c9e8d3cab9e1c5b5
yezi <yehaowei20060411@qq.com>
2024-06-24 22:27:21 +0800

    feat: 添加 `parse_sort_string` 函数

---
+++ sshared/api/utils.py
@@ -0,0 +1,16 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sshared.mongo.document import SortType
+
+
+def parse_sort_string(string: str, /) -> "SortType":
+    result: SortType = {}
+
+    for item in string.split(","):
+        if item.startswith("-"):
+            result[item[1:]] = "DESC"
+        else:
+            result[item] = "ASC"
+
+    return result
