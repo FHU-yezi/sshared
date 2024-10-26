d028a67101c308ed0d4c024ce1c00509b03338a0
FHU-yezi <yehaowei20060411@qq.com>
2024-10-26 15:01:35 +0800

    feat: `postgres` 模块添加 `ConnectionManager` 类

---
+++ sshared/postgres/connection_manager.py
@@ -0,0 +1,66 @@
+from asyncio import sleep
+from typing import Optional
+
+from psycopg import AsyncConnection, OperationalError
+
+
+class ConnectionManager:
+    def __init__(self, connection_string: str) -> None:
+        self._connection_string = connection_string
+        self._conn: Optional[AsyncConnection] = None
+        self._connecting = False
+
+    async def _blocking_connect(self) -> None:
+        self._connecting = True
+
+        while True:
+            try:
+                self._conn = await AsyncConnection.connect(
+                    self._connection_string, autocommit=True
+                )
+                self._connecting = False
+                return
+            except OperationalError:  # noqa: PERF203
+                await sleep(0.1)
+
+    async def _blocking_waiting_for_conn(self) -> AsyncConnection:
+        while True:
+            if self._conn:
+                return self._conn
+
+            await sleep(0.03)
+
+    async def _check(self) -> bool:
+        if not self._conn:
+            return False
+
+        try:
+            await self._conn.execute("")
+            return True
+        except OperationalError:
+            return False
+
+    async def get_conn(self) -> AsyncConnection:
+        # 尚未连接
+        if not self._conn:
+            # 如果正在尝试连接，阻塞等待
+            if self._connecting:
+                return await self._blocking_waiting_for_conn()
+
+            # 否则，尝试连接并返回
+            await self._blocking_connect()
+            return self._conn  # type: ignore
+
+        # 已连接，检查状态
+        ok = await self._check()
+        # 如果状态异常，重新连接
+        if not ok:
+            # 如果正在尝试连接，阻塞等待
+            if self._connecting:
+                return await self._blocking_waiting_for_conn()
+
+            # 否则，尝试连接
+            await self._blocking_connect()
+
+        # 状态正常，返回
+        return self._conn  # type: ignore
