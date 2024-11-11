755488b09207fcc61db36aa278478d1b330f7a3c
FHU-yezi <yehaowei20060411@qq.com>
2024-11-11 08:31:10 +0800

    feat: 添加 `parse_td_str` 函数

--- sshared/time.py
+++ sshared/time.py
@@ -9,5 +9,20 @@ def get_now_without_microsecond() -> datetime:
     return datetime.now().replace(microsecond=0)
 
 
+def parse_td_str(td_str: str) -> timedelta:
+    value, unit = int(td_str[:-1]), td_str[-1]
+
+    if unit == "s":
+        return timedelta(seconds=value)
+    if unit == "m":
+        return timedelta(minutes=value)
+    if unit == "h":
+        return timedelta(hours=value)
+    if unit == "d":
+        return timedelta(days=value)
+
+    raise ValueError
+
+
 def get_datetime_before_now(td: timedelta) -> datetime:
     return datetime.now() - td
